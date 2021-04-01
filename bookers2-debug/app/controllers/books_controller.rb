f83c747a5aeb0e95c7fce5e53ec36df234e6d76e
mi-1109 <meisuzuki.sophia@gmail.com>
2021-04-01 09:03:34 +0000

    各機能のデバッグ完了

--- bookers2-debug/app/controllers/books_controller.rb
+++ bookers2-debug/app/controllers/books_controller.rb
@@ -2,15 +2,18 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
+    @newbook = Book.new
   end
 
   def index
+    @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def create
     @book = Book.new(book_params)
-    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -23,8 +26,6 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-
-
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -34,16 +35,16 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body).merge(user_id: current_user.id)
   end
 
 end
